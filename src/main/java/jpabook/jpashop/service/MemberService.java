2b5f0f1c713e5c970d7b4535f66f094458e3dd40
kwook <kwook@isu.co.kr>
2023-05-02 08:10:55 +0900

    2023.05.02 commit

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 체크
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+    }
+
+    //회원 전체조회
+}
